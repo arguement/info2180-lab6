9befa0ee838e76cf521a2540b993181cf5f808f5
Jordan Williams <jordan.williams02@mymona.uwi.edu>
2018-11-09 07:26:57 +0000

    second

--- control.js
+++ control.js
@@ -1,17 +1,19 @@
 // JavaScript File
 window.onload = function(){
     let button = document.getElementById("submission");
+    let answer = document.getElementById("result");
     
     button.addEventListener("click",function(){
+        let text =document.getElementById("text").value;
         
         let xml = new XMLHttpRequest();
         xml.onreadystatechange = function(){
             if (this.readyState ==4 && this.status == 200){
                 let response = xml.responseText;
-                alert(response);
+                answer.innerHTML = response;
             }
         }
-        xml.open("GET", "request.php?q=definition", true);
+        xml.open("GET", "request.php?q="+text, true);
         xml.send();
     });
 };
