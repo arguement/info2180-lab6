e90407da2b721ba6c41417a0611b284ee72da360
Jordan Williams <jordan.williams02@mymona.uwi.edu>
2018-11-11 22:03:57 +0000

    finish

--- control.js
+++ control.js
@@ -1,7 +1,11 @@
 // JavaScript File
+
 window.onload = function(){
+    
     let button = document.getElementById("submission");
     let answer = document.getElementById("result");
+    let a = document.createElement("ul");
+    let alls = document.getElementById("all");
     
     button.addEventListener("click",function(){
         let text =document.getElementById("text").value;
@@ -11,9 +15,34 @@ window.onload = function(){
             if (this.readyState ==4 && this.status == 200){
                 let response = xml.responseText;
                 answer.innerHTML = response;
+                
             }
         }
         xml.open("GET", "request.php?q="+text, true);
         xml.send();
     });
+    
+    
+    alls.addEventListener("click",function(){
+        let xmls = new XMLHttpRequest();
+        xmls.onreadystatechange = function(){
+            if (this.readyState ==4 && this.status == 200){
+                let response = xmls.responseText;
+                
+                answer.innerHTML = response;
+                
+            }
+        }
+        xmls.open("GET", "request.php?q=&all="+true, true);
+        xmls.send();
+    });
+   
+    
+    function createNode(text){
+        
+        let j = document.createElement("li");
+        j.innerHTML = text.nodeValue;
+        a.append(j);
+        
+    }
 };
--- request.php
+++ request.php
@@ -4,6 +4,9 @@
 // respond with a value
 
 $query = $_GET['q'];
+$new = $_GET['all'];
+
+
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -14,5 +17,57 @@ $definition = [
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
 
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+
+    <definition name="definition" author="John">
+A statement of the exact meaning of a word, especially in a dictionary.
+    </definition>
+    
+    <definition name="bar" author="mary">
+A place that sells alcholic beverages
+    </definition>
+    
+    <definition name="ajax" author="Kimberly">
+Technique which involves the use of javascript and xml (or JSON)
+    </definition>
+    
+    <definition name="html" author="Kimberly">
+The standard markup language for creating web pages and web applications.
+    </definition>
+     <definition name="css" author="Kimberly">
+A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+    <definition name="javascript" author="Kimberly">
+A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+    <definition name="php" author="Kimberly">
+A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+    </definition>
+</entries>';
+if (isset($query) and empty($query) == false){
+
 print "<h3>" . strtoupper($query) . "</h3>";
 print "<p>" . $definition[$query] . "</p>";
+    
+}
+
+if (isset($new)){
+  header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+  
+print("<ol>");
+
+foreach($xmlOutput as $some){
+    print("<li>");
+    
+    print "<h3>" . $some["name"] . "</h3>";
+    print "<p>" . $some . "</p>";
+    print "<p>" . "----".$some["author"] . "</p>";
+    print("</li>");
+}
+
+print("</ol>");
+   
+    
+}
